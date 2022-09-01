7c1f8db167a81d75638c738cacd004ac3b62d8db
Mason Stuart <mason.stuart05@outlook.com>
2022-09-01 12:36:21 -0700

    More JNI FTC port code

--- FTC/Matrix.java
+++ FTC/Matrix.java
@@ -2,4 +2,8 @@ class Matrix {
     protected native void print();
 
     protected native void transpose();
+
+    protected native Matrix add(Matrix other);
+
+    protected native Matrix subtract(Matrix other);
 }
