ce5c42e44ad273fe9d9279368fc6280476f1553c
Mason Stuart <mason.stuart05@outlook.com>
2022-09-01 17:05:43 -0700

    JNI stuff

--- FTC/Matrix.java
+++ FTC/Matrix.java
@@ -1,25 +1,9 @@
-class Matrix {
-    protected native void print();
-
+public class Matrix {
     protected native void transpose();
 
     protected native Matrix add(Matrix other);
 
     protected native Matrix subtract(Matrix other);
-
-    protected native void add(Matrix other);
-
-    protected native void subtract(Matrix other);
-
-    // constant math
-    protected native void add(double num);
-
-    protected native void subtract(double num);
-
-    protected native void multiply(double num);
-
-    protected native void divide (double num);
-
 
     public void print() {
         System.out.println("Hello World!");
