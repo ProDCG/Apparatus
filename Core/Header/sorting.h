caec98c3617e13423e78eea646225ce2eeaedb41
Mason Stuart <mason.stuart05@outlook.com>
2022-09-05 09:01:02 -0700

    some wonky stuff

--- Core/Header/sorting.h
+++ Core/Header/sorting.h
@@ -2,22 +2,13 @@
 #define SORTING_H
 
 #include <array>
-// POLICY PATTERN
-// in implementation of sort() do the desired policy 
 
-template<typename T, size_t s>
-struct sort {
-    virtual std::array<T, s> sort(std::array<T, s> arr);
-};
+namespace sorting {
+    template<typename T, size_t s>
+    std::array<T, s> quick_sort(std::array<T, s> arr);
 
+    template<typename T, size_t s>
+    std::array<T, s> merge_sort(std::array<T, s> arr);
+}
 
-
-// template<typename policy, typename T, size_t s> std::array<T, s> sort(std::array<T, s> arr);
-
-// struct insertion_sort_policy_t {    
-// };
-
-// struct merge_sort_policy_t {};
-
-// struct bubble_sort_policy_t {};
 #endif
--- Core/Source/mergesort.cpp
+++ Core/Source/mergesort.cpp
@@ -2,7 +2,7 @@
 
 namespace sorting {
     template<typename T, size_t s>
-    std::array<T, s> quick_sort(std::array<T, s> arr) {
+    std::array<T, s> merge_sort(std::array<T, s> arr) {
         return arr;
     }
 }
