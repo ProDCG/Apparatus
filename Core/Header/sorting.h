c6dfbb66dabc746e6f1dabddbc02bcd9bb783a88
Mason Stuart <mason.stuart05@outlook.com>
2022-09-05 08:34:36 -0700

    wtf

--- Core/Header/sorting.h
+++ Core/Header/sorting.h
@@ -5,14 +5,19 @@
 // POLICY PATTERN
 // in implementation of sort() do the desired policy 
 
-template<typename policy, typename T, size_t s> std::array<T, s> sort(std::array<T, s> arr);
-
-struct insertion_sort_policy_t {
-    // something about defining insertion sort
-    
+template<typename T, size_t s>
+struct sort {
+    virtual std::array<T, s> sort(std::array<T, s> arr);
 };
 
-struct merge_sort_policy_t {};
 
-struct bubble_sort_policy_t {};
+
+// template<typename policy, typename T, size_t s> std::array<T, s> sort(std::array<T, s> arr);
+
+// struct insertion_sort_policy_t {    
+// };
+
+// struct merge_sort_policy_t {};
+
+// struct bubble_sort_policy_t {};
 #endif
