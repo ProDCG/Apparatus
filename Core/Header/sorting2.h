bdc9ca9945bc8ec9f3176325b29224a846367b8e
Mason Stuart <mason.stuart05@outlook.com>
2022-09-05 16:22:31 -0700

    scuffed

--- Core/Header/sorting2.h
+++ Core/Header/sorting2.h
@@ -1,12 +1,24 @@
 #ifndef SORTING2_H
 #define SORTING2_H
 
+#include <iostream>
 #include <array>
+
 // POLICY PATTERN
 // in implementation of sort() do the desired policy 
 
-template<typename policy, typename T, size_t n>
-std::array<T, n> sort(std::array<T, n> arr);
+template <typename T>
+T sort(T arr);
 
-struct insertion_sort_policy_t {};
+struct insertion_sort_policy_t {
+    template<typename T>
+    T operator()(T arr) {
+        return arr;
+    }
+};
+
+std::array<int, 3> arr = {1, 2, 3};
+arr = sort<insertion_sort_policy_t>(arr);
+
+
 #endif
