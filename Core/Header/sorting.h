97d3dfe49e601cc323ccc32228834a02bb5f8752
Mason Stuart <mason.stuart05@outlook.com>
2022-09-04 23:04:30 -0700

    Insertion sort declaration

--- Core/Header/sorting.h
+++ Core/Header/sorting.h
@@ -4,6 +4,9 @@
 #include <array>
 
 template<typename T, size_t s>
-std::array<T, s> sort(std::array<T, s> arr);
+std::array<T, s> mergeSort(std::array<T, s> arr);
+
+template<typename T, size_t s>
+std::array<T, s> insertionSort(std::array<T, s> arr);
 
 #endif
