63d53f43bcd315fbba02c9170bc9de03bf51ff04
Mason Stuart <mason.stuart05@outlook.com>
2022-09-04 23:47:39 -0700

    Policy pattern

--- Core/Header/sorting.h
+++ Core/Header/sorting.h
@@ -9,4 +9,11 @@ std::array<T, s> mergeSort(std::array<T, s> arr);
 template<typename T, size_t s>
 std::array<T, s> insertionSort(std::array<T, s> arr);
 
+// POLICY PATTERN
+// in implementation of sort() do the desired policy
+
+template<typename policy, typename T> T sort(T arr);
+struct insertion_sort_policy_t {
+    // something about defining insertion sort
+};
 #endif
