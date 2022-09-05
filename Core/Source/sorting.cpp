92f006f7906d364ac384961bccafde9ea38fed9c
Mason Stuart <mason.stuart05@outlook.com>
2022-09-05 07:35:07 -0700

    pre-template redo test

--- Core/Source/sorting.cpp
+++ Core/Source/sorting.cpp
@@ -1,1 +1,8 @@
 #include "sorting.h"
+
+template<typename policy, typename T> T sort(T arr) {
+    for (int i = 0; i < arr.length; i++) {
+        std::cout << i << std::endl;
+    }
+    return arr;
+}
--- Examples/SortingDemo.cpp
+++ Examples/SortingDemo.cpp
@@ -1,4 +1,3 @@
 #include "sorting.h"
 
 int values[] = {1, 2, 3};
-values = sort(insertion_sort_policy_t, values);
