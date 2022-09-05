514c1f29ca5285d42d904598fe2968539d8294b5
Mason Stuart <mason.stuart05@outlook.com>
2022-09-05 16:33:36 -0700

    Thank you Max ur awesome

--- Core/Header/sorting2.h
+++ Core/Header/sorting2.h
@@ -4,21 +4,22 @@
 #include <iostream>
 #include <array>
 
-// POLICY PATTERN
-// in implementation of sort() do the desired policy 
+template <typename Policy, typename T> 
+void sort(T arr);
 
-template <typename T>
-T sort(T arr);
-
-struct insertion_sort_policy_t {
+struct quick_sort_policy_t {
     template<typename T>
-    T operator()(T arr) {
+    void operator()(T arr) {
         return arr;
     }
 };
 
 std::array<int, 3> arr = {1, 2, 3};
-arr = sort<insertion_sort_policy_t>(arr);
+
+int main() {
+    sort<quick_sort_policy_t>(arr);
+}
+
 
 
 #endif
