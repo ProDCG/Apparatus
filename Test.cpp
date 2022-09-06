0e617a20ca2ab89dff9781b379bc07904570f9db
Mason Stuart <mason.stuart05@outlook.com>
2022-09-06 07:57:31 -0700

    Type ID stuff

--- Test.cpp
+++ Test.cpp
@@ -9,13 +9,14 @@ void sort(T arr);
 struct quick_sort_policy_t {
     template<typename T>
     void operator()(T& arr) {
-        std::cout << arr << '\n';
+        std::cout << typeid(arr).name() << '\n';
     }
 };
 
 std::array<int, 3> arr = {1, 2, 3};
+int arr2[] = {1, 2, 3};
 
 int main() {
-    sort<quick_sort_policy_t>(&arr);
+    sort<quick_sort_policy_t>(&arr2);
     std::cout << arr.at(1);
 }
