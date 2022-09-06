f2de1a11ad82af50a7731bb452dd17a52251ba8e
Mason Stuart <mason.stuart05@outlook.com>
2022-09-06 10:17:26 -0700

    adding sort impls in

--- Core/Source/sorting.cpp
+++ Core/Source/sorting.cpp
@@ -1,2 +1,15 @@
 #include <sorting.h>
 
+struct insertion_sort_policy_t {
+    template <typename T>
+    void operator()(T& arr) {
+        // sort
+    }
+};
+
+struct merge_sort_policy_t {
+    template <typename T>
+    void operator()(T& arr) {
+        // sort
+    }
+};
