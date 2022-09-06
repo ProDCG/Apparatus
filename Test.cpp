29aa27a4a3885b6fbf54b4ce6ad534cd33effccc
Mason Stuart <mason.stuart05@outlook.com>
2022-09-06 14:14:46 -0700

    Policies

--- Test.cpp
+++ Test.cpp
@@ -6,32 +6,9 @@
 #include <iostream>
 #include <array>
 
-template <typename Policy, typename T> 
-void sort(T& arr) {
-    Policy p;
-
-    p(arr);
-}
-
-struct some_sort_policy {
-    template<typename T>
-    void operator()(T& arr) {
-        std::cout << typeid(arr).name() << '\n';
-        arr[1] = 15;
-    }
-};
-
-struct some_other_sort_policy {
-    template<typename T>
-    void operator()(T& arr) {
-        std::cout << "dab";
-    }
-};
-
-
 std::array<int, 3> arr = {1, 2, 3};
 
 int main() {
-    sort<some_other_sort_policy>(arr);
+    sort<merge_sort_policy_t>(arr);
     std::cout << arr[1];
 }
