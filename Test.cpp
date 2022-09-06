623dd34aed718030f606417ad5f700aeb207794a
Mason Stuart <mason.stuart05@outlook.com>
2022-09-06 09:42:29 -0700

    thx max u suck

--- Test.cpp
+++ Test.cpp
@@ -4,25 +4,31 @@
 #include <array>
 
 template <typename Policy, typename T> 
-void sort(T arr);
+void sort(T& arr) {
+    Policy p;
+
+    p(arr);
+}
 
 struct some_sort_policy {
     template<typename T>
     void operator()(T& arr) {
         std::cout << typeid(arr).name() << '\n';
-        // do something with the array
+        arr[1] = 15;
     }
 };
 
+struct some_other_sort_policy {
+    template<typename T>
+    void operator()(T& arr) {
+        std::cout << "dab";
+    }
+};
+
+
 std::array<int, 3> arr = {1, 2, 3};
-int arr2[] = {1, 2, 3};
 
 int main() {
-    sort<some_sort_policy>(&arr2);
-    std::cout << arr2[1];
-}
-
-template<typename Policy, typename T>
-void sort(T arr) {
-    std::cout << typeid(arr).name() << '\n';
+    sort<some_other_sort_policy>(arr);
+    std::cout << arr[1];
 }
