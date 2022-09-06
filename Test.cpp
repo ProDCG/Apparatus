0b5670330de69e6d0d80c08dddaac12cd6114830
Mason Stuart <mason.stuart05@outlook.com>
2022-09-06 08:10:01 -0700

    how to WORK

--- Test.cpp
+++ Test.cpp
@@ -19,10 +19,10 @@ int arr2[] = {1, 2, 3};
 
 int main() {
     sort<some_sort_policy>(&arr2);
-    std::cout << arr.at(1);
+    std::cout << arr2[1];
 }
 
 template<typename Policy, typename T>
 void sort(T arr) {
-    std::cout << "a" << std::endl;
+    std::cout << typeid(arr).name() << '\n';
 }
