35f2b00194faada383968c956b2e8f5369e19109
Mason Stuart <mason.stuart05@outlook.com>
2022-09-07 12:22:06 -0700

    bubble + selection sort

--- Core/Source/sorting.cpp
+++ Core/Source/sorting.cpp
@@ -13,3 +13,17 @@ struct merge_sort_policy_t {
         // sort
     }
 };
+
+struct bubble_sort_policy_t {
+    template <typename T>
+    void operator()(T& arr) {
+        // sort
+    }
+};
+
+struct selection_sort_policy_t {
+    template <typename T>
+    void operator()(T& arr) {
+        // sort
+    }
+};
