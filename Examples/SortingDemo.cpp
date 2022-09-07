52fd08467bf42776370a2301c73e09054d00d248
Mason Stuart <mason.stuart05@outlook.com>
2022-09-07 13:00:42 -0700

    Execution time testing

--- Examples/SortingDemo.cpp
+++ Examples/SortingDemo.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include <array>
 #include <sorting.h>
+#include <chrono>
 
 std::array<int, 6> values = {1, 29, 643, 25, -5};
 int main() {
-    for (int num : values) {
-        std::cout << num << ", ";
-    }
-    std::cout << '\n';
+    
     sort<insertion_sort_policy_t>(values);
-    for (int num : values) {
-        std::cout << num << ", ";
-    }
 }
