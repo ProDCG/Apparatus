30a322fb61582f2d087c0445768860c4080b6fd5
Mason Stuart <mason.stuart05@outlook.com>
2022-09-07 16:34:39 -0700

    stuff

--- Examples/InsertionSort.java
+++ Examples/InsertionSort.java
@@ -3,12 +3,12 @@ package Examples;
 class InsertionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 29, 643, 25, -5};
-        System.out.println(arr);
+        System.out.println(arr[0]);
     }
 
     public void insertion(int[] arr) {
-        int i, key, j;
-        for (i = 1; i < arr.length; i++) {
+        int key, j;
+        for (int i = 1; i < arr.length; i++) {
             key = arr[i];
             j = i - 1;
 
