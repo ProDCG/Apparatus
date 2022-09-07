ae8e53a6d2cf0584b460681a040fcb7b8b2635fd
Mason Stuart <mason.stuart05@outlook.com>
2022-09-07 12:50:53 -0700

    moving around files

--- Examples/SortingDemo.cpp
+++ Examples/SortingDemo.cpp
@@ -2,4 +2,5 @@
 #include <sorting.h>
 
 std::array<int, 6> values = {1, 29, 643, 25, -5};
-std::array<int, 5> values3 = sort<insertion_sort_policy_t>(values);
+sort<insertion_sort_policy_t>(values);
+std::cout << values << std::endl;
