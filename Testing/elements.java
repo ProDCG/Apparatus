b11e77adb2fbcd724ce7269ff666389503eee97c
Mason Stuart <mason.stuart05@outlook.com>
2022-09-07 18:07:48 -0700

    more execution time testing

--- Testing/elements.java
+++ Testing/elements.java
@@ -11,10 +11,11 @@ public class elements {
     public static void main(String[] args) throws IOException {
         FileWriter myWriter = new FileWriter("elements.txt");
         Random rand = new Random();
-        myWriter.write("[");
-        for (int i = 0; i < 10000; i++) {
+        myWriter.write("{");
+        for (int i = 0; i < 1000; i++) {
             myWriter.write(rand.nextInt(10001) + ", ");
         }
+        myWriter.write("}");
         myWriter.close();
     }
     
