03d2c95b7b5ec874c4e7f13aa2c47d43dcac008a
Mason Stuart <mason.stuart05@outlook.com>
2022-09-08 10:35:44 -0700

    reimplementing quick sort

--- Testing/InsertionSort.java
+++ Testing/InsertionSort.java
@@ -1,4 +1,4 @@
-package Examples;
+package Testing;
 
 class InsertionSort {
 
@@ -8,24 +8,62 @@ class InsertionSort {
         long timeTotal = 0;
         for (int i = 0; i < 100; i++) {
             long startTime = System.nanoTime();
-            insertion(arr);
+            quickSort(arr, 0, arr.length - 1);
             long endTime = System.nanoTime();
             timeTotal += ((endTime - startTime) / 100000);
         }
         System.out.println( (float)timeTotal / 100);
     }
 
-    public static void insertion(int[] arr) {
-        int key, j;
-        for (int i = 1; i < arr.length; i++) {
-            key = arr[i];
-            j = i - 1;
+    static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
+    static int partition(int[] arr, int low, int high)
+{
+      
+    // pivot
+    int pivot = arr[high]; 
+      
+    // Index of smaller element and
+    // indicates the right position
+    // of pivot found so far
+    int i = (low - 1); 
+  
+    for(int j = low; j <= high - 1; j++)
+    {
+          
+        // If current element is smaller 
+        // than the pivot
+        if (arr[j] < pivot) 
+        {
+              
+            // Increment index of 
+            // smaller element
+            i++; 
+            swap(arr, i, j);
         }
     }
+    swap(arr, i + 1, high);
+    return (i + 1);
+}
+
+static void quickSort(int[] arr, int low, int high)
+{
+    if (low < high) 
+    {
+          
+        // pi is partitioning index, arr[p]
+        // is now at right place 
+        int pi = partition(arr, low, high);
+  
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
 }
