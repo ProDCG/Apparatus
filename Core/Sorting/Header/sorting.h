821f1bce99c9c2fb6c062db1bda39abba8b2eb13
Mason Stuart <mason.stuart05@outlook.com>
2022-09-10 07:56:14 -0700

    Quick sort go brrrrrrrrrrrrrrrr

--- Core/Sorting/Header/sorting.h
+++ Core/Sorting/Header/sorting.h
@@ -39,12 +39,31 @@ struct quick_sort_policy_t {
 
     template<typename T>
     void quicksort(T& arr, int low, int high) {
-        \
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quicksort(arr, low, pi - 1);
+            quicksort(arr, pi + 1, high);
+        }
     }
 
     template<typename T>
-    void swap(T *a, T *b) {
-        T t = *a;
+    int partition(T& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(&arr[i], &arr[j]);
+            }
+        }
+        swap(&arr[i + 1], &arr[high]);
+        return (i + 1);
+    }
+
+    void swap(int *a, int *b) {
+        int t = *a;
         *a = *b;
         *b = t;
     }
