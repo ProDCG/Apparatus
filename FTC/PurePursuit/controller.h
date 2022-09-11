fee9504fbbc538e5ddd168e33f8476bf9efb155d
Mason Stuart <mason.stuart05@outlook.com>
2022-09-11 09:20:06 -0700

    pure pursuit stuff

--- FTC/PurePursuit/controller.h
+++ FTC/PurePursuit/controller.h
@@ -2,10 +2,15 @@
 #define PURE_PURSUIT_CONTROLLER_H
 
 namespace controller {
-    template <typename Point>
-void lineCircleIntersection(Point pointA, Point pointB, Point center, double radius) {
+    template <typename Pose>
+    Pose relativeDistanceToTarget(Pose self, Pose target) {
+        return target.subtract(self);
+    }
 
-}
+    template <typename Point>
+    void lineCircleIntersection(Point pointA, Point pointB, Point center, double radius) {
+
+    }
 }
 
 
