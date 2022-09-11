3aac18ed786f1a877267d5aaea3e62c1dcf82ab8
Mason Stuart <mason.stuart05@outlook.com>
2022-09-10 23:17:48 -0700

    profiling with velocity

--- FTC/PurePursuit/Geometry/profiling.h
+++ FTC/PurePursuit/Geometry/profiling.h
@@ -1,8 +1,18 @@
 #ifndef PURE_PURSUIT_GEOMETRY_PROFILING_H
 #define PURE_PURSUIT_GEOMETRY_PROFILING_H
 
+#include <algorithm>
+
 namespace profiling {
-    double update(double time, double maxAcceleration, double maxVelocity);
+    double update(double time, double maxAcceleration, double maxVelocity) {
+        if (time < 0) {
+            return 0;
+        }
+
+        double velocity = (maxAcceleration * time) / maxVelocity;
+        velocity = std::min(velocity, maxVelocity);
+        return velocity;
+    }
 }
 
 #endif
