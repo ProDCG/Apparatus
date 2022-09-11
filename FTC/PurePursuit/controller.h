d47ed0b1ae121df314e3ad8a616c76d096695cb2
Mason Stuart <mason.stuart05@outlook.com>
2022-09-10 23:08:32 -0700

    pure pursuit stuff

--- FTC/PurePursuit/controller.h
+++ FTC/PurePursuit/controller.h
@@ -1,4 +1,9 @@
 #ifndef PURE_PURSUIT_CONTROLLER_H
 #define PURE_PURSUIT_CONTROLLER_H
 
+template <typename Point>
+void lineCircleIntersection(Point pointA, Point pointB, Point center, double radius) {
+
+}
+
 #endif
