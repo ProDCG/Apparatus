03a492c8677c7bd1a6d4b49643a721ebf01b5fff
Mason Stuart <mason.stuart05@outlook.com>
2022-09-10 23:15:06 -0700

    motion profiling p2

--- FTC/PurePursuit/Geometry/profiling.h
+++ FTC/PurePursuit/Geometry/profiling.h
@@ -1,4 +1,8 @@
 #ifndef PURE_PURSUIT_GEOMETRY_PROFILING_H
 #define PURE_PURSUIT_GEOMETRY_PROFILING_H
 
+namespace profiling {
+    double update(double time, double maxAcceleration, double maxVelocity);
+}
+
 #endif
