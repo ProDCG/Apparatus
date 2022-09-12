33bc4f91ad9e72a19976d30a1837874c9c706638
Mason Stuart <mason.stuart05@outlook.com>
2022-09-12 14:09:10 -0700

    Matrix re-declaration

--- Core/LinearAlgebra/matrix_2x2.hpp
+++ Core/LinearAlgebra/matrix_2x2.hpp
@@ -4,5 +4,8 @@
 
 namespace Apparatus {
 
-    typedef Matrix<3, 3> mat3;
+    typedef Matrix<3, 4> mat3x4;
+    typedef Matrix<3, 3> mat3x3;
+    typedef Matrix<3, 2> mat3x2;
+    typedef Matrix<3, 1> mat3x1;
 }
