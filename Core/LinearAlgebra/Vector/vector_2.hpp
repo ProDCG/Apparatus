d4d91853a742c4eb3b840c3f622f9eab802243ba
Mason Stuart <mason.stuart05@outlook.com>
2022-09-13 10:26:11 -0700

    Changed generic and vec2

---
+++ Core/LinearAlgebra/Vector/vector_2.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <../Core/LinearAlgebra/Vector/vectors.hpp>
+
+namespace apparatus {
+
+    typedef Vector<2, int> i_vec2;
+    typedef Vector<2, double> d_vec2, vec2;
+    typedef Vector<2, bool> b_vec2;
+}
