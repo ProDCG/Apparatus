af445601d098d576b5bb7be293ffa4134a036515
Mason Stuart <mason.stuart05@outlook.com>
2022-09-12 20:53:23 -0700

    switching imports

---
+++ Core/LinearAlgebra/matrix_2x3.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <../Core/LinearAlgebra/matrix_generic.hpp>
+
+namespace apparatus {
+
+    typedef Matrix<2, 2, int> i_mat2x2, i_mat2;
+    typedef Matrix<2, 2, double> d_mat2x2, d_mat2;
+    typedef Matrix<2, 2, double> mat2x2, mat2;
+    typedef Matrix<2, 2, bool> b_mat2x2, b_mat2;
+}
