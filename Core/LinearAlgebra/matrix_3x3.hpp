7d279b86c61227382a8039587e9bb7035490c465
Mason Stuart <mason.stuart05@outlook.com>
2022-09-12 20:59:58 -0700

    3x3 matrix

---
+++ Core/LinearAlgebra/matrix_3x3.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <../Core/LinearAlgebra/matrix_generic.hpp>
+
+namespace apparatus {
+
+    typedef Matrix<3, 3, int> i_mat3x3;
+    typedef Matrix<3, 3, double> d_mat3x3;
+    typedef Matrix<3, 3, double> mat3x3;
+    typedef Matrix<3, 3, bool> b_mat3x3;
+}
