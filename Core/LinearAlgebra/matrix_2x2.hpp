81ef54d70827b446d97bba09d5deef379962eda2
Mason Stuart <mason.stuart05@outlook.com>
2022-09-13 07:41:18 -0700

    modifying 2x2 matrices

--- Core/LinearAlgebra/matrix_2x2.hpp
+++ Core/LinearAlgebra/matrix_2x2.hpp
@@ -5,7 +5,6 @@
 namespace apparatus {
 
     typedef Matrix<2, 2, int> i_mat2x2, i_mat2;
-    typedef Matrix<2, 2, double> d_mat2x2, d_mat2;
-    typedef Matrix<2, 2, double> mat2x2, mat2;
+    typedef Matrix<2, 2, double> d_mat2x2, d_mat2, mat2x2, mat2;
     typedef Matrix<2, 2, bool> b_mat2x2, b_mat2;
 }
