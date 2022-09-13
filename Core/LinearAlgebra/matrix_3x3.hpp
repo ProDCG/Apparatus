cb7cfcc8b6422145cfd1bbe1d0a9b5cab1622da0
Mason Stuart <mason.stuart05@outlook.com>
2022-09-13 07:42:36 -0700

    Added other keywords

--- Core/LinearAlgebra/matrix_3x3.hpp
+++ Core/LinearAlgebra/matrix_3x3.hpp
@@ -4,7 +4,7 @@
 
 namespace apparatus {
 
-    typedef Matrix<3, 3, int> i_mat3x3;
+    typedef Matrix<3, 3, int> i_mat3x3, i_mat3;
     typedef Matrix<3, 3, double> d_mat3x3, d_mat3, mat3x3, mat3;
-    typedef Matrix<3, 3, bool> b_mat3x3;
+    typedef Matrix<3, 3, bool> b_mat3x3, b_mat3;
 }
--- Core/LinearAlgebra/matrix_4x4.hpp
+++ Core/LinearAlgebra/matrix_4x4.hpp
@@ -6,5 +6,5 @@ namespace apparatus {
 
     typedef Matrix<4, 4, int> i_mat4x4, i_mat4;
     typedef Matrix<4, 4, double> d_mat4x4, d_mat4, mat4x4, mat4;
-    typedef Matrix<4, 4, bool> b_mat4x4;
+    typedef Matrix<4, 4, bool> b_mat4x4, b_mat4;
 }
