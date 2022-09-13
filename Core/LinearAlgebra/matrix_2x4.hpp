2e21a7e1922107dd0bb923d08255e2b94abc1e71
Mason Stuart <mason.stuart05@outlook.com>
2022-09-12 20:56:50 -0700

    2x4 matrix

---
+++ Core/LinearAlgebra/matrix_2x4.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <../Core/LinearAlgebra/matrix_generic.hpp>
+
+namespace apparatus {
+
+    typedef Matrix<2, 4, int> i_mat2x4;
+    typedef Matrix<2, 4, double> d_mat2x4;
+    typedef Matrix<2, 4, double> mat2x4;
+    typedef Matrix<2, 4, bool> b_mat2x4;
+}
