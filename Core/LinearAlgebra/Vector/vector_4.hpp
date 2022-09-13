e276f1f56012391931d11a7a269c5764ef4d878d
Mason Stuart <mason.stuart05@outlook.com>
2022-09-13 10:30:01 -0700

    Added vec4

---
+++ Core/LinearAlgebra/Vector/vector_4.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <../Core/LinearAlgebra/Vector/vectors.hpp>
+
+namespace apparatus {
+
+    typedef Vector<4, int> i_vec4;
+    typedef Vector<4, double> d_vec4, vec4;
+    typedef Vector<4, bool> b_vec4;
+}
