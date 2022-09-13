3a177634ba5b3796569fb939114c434055fd2e06
Mason Stuart <mason.stuart05@outlook.com>
2022-09-12 20:56:23 -0700

    2x3 matrix fix

--- Core/LinearAlgebra/matrix_2x3.hpp
+++ Core/LinearAlgebra/matrix_2x3.hpp
@@ -7,5 +7,5 @@ namespace apparatus {
     typedef Matrix<2, 3, int> i_mat2x3;
     typedef Matrix<2, 3, double> d_mat2x3;
     typedef Matrix<2, 3, double> mat2x3;
-    typedef Matrix<2, 3, bool> b_mat2x3
+    typedef Matrix<2, 3, bool> b_mat2x3;
 }
