131079c47b29b4d421216e02f6f06e5ec4e318d6
Mason Stuart <mason.stuart05@outlook.com>
2022-09-13 07:40:55 -0700

    modifying 3x3 matrices

--- Core/LinearAlgebra/matrix_3x3.hpp
+++ Core/LinearAlgebra/matrix_3x3.hpp
@@ -5,7 +5,6 @@
 namespace apparatus {
 
     typedef Matrix<3, 3, int> i_mat3x3;
-    typedef Matrix<3, 3, double> d_mat3x3;
-    typedef Matrix<3, 3, double> mat3x3;
+    typedef Matrix<3, 3, double> d_mat3x3, d_mat3, mat3x3, mat3;
     typedef Matrix<3, 3, bool> b_mat3x3;
 }
