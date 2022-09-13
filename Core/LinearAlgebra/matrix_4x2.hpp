015bf01f1dc0b433afe4b5f1ab4da7359867e445
Mason Stuart <mason.stuart05@outlook.com>
2022-09-12 21:02:19 -0700

    4x2 matrix

---
+++ Core/LinearAlgebra/matrix_4x2.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <../Core/LinearAlgebra/matrix_generic.hpp>
+
+namespace apparatus {
+
+    typedef Matrix<4, 2, int> i_mat4x2;
+    typedef Matrix<4, 2, double> d_mat4x2;
+    typedef Matrix<4, 2, double> mat4x2;
+    typedef Matrix<4, 2, bool> b_mat4x2;
+}
