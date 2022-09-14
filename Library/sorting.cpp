9166c1dcf50ebb75e1ac0300591c5fc82cc6cfbd
Mason Stuart <mason.stuart05@outlook.com>
2022-09-14 09:39:44 -0700

    Missed partition function lol

--- Library/sorting.cpp
+++
@@ -1,2 +0,0 @@
-#include <Core/Header/sorting.h>
-
--- Library/sorting.h
+++ Library/sorting.h
@@ -100,6 +100,27 @@ struct quick_sort_iterative_policy_t {
             }
         }
     }
+
+    template<typename T>
+    int partition(T& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(&arr[i], &arr[j]);
+            }
+        }
+        swap(&arr[i + 1], &arr[high]);
+        return (i + 1);
+    }
+
+    void swap(int *a, int *b) {
+        int t = *a;
+        *a = *b;
+        *b = t;
+    }
 };
 
 struct selection_sort_policy_t {
