e722c5b451d1ee184b5f7a901de57c48f0d0df9b
Mason Stuart <mason.stuart05@outlook.com>
2022-09-16 10:24:35 -0700

    operator overloading

--- Core/LinearAlgebra/Testing/vector2.cpp
+++ Core/LinearAlgebra/Testing/vector2.cpp
@@ -5,16 +5,21 @@ app::Vector<T, C>::Vector(std::initializer_list<T> list) {
     std::copy(list.begin(), list.end(), app::Vector<T, C>::vec);
 }
 
-template <typename T, size_t C>
-app::Vector<T, C> app::operator+(const T& rhs) {
-    std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
-        element += rhs;
-    });
-}
+// template <typename T, size_t C>
+// app::Vector<T, C> app::operator+(const T& rhs) {
+//     std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
+//         element += rhs;
+//     });
+// }
+
+// template <typename T, size_t C>
+// void app::operator+=(Vector<T, C> lhs, const T& rhs) {
+//     std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
+//         element += rhs;
+//     });
+// }
 
 template <typename T, size_t C>
-void app::operator+=(Vector<T, C> lhs, const T& rhs) {
-    std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
-        element += rhs;
-    });
+app::Vector<T, C> app::Vector<T, C>::operator+() {
+
 }
--- Core/LinearAlgebra/Testing/vector2.hpp
+++ Core/LinearAlgebra/Testing/vector2.hpp
@@ -7,14 +7,19 @@ namespace app {
     template <typename T, size_t C> class Vector{
         private:
             // std::array<T, C> vec;
+            Vector<T, C> operator+(Vector<T, C>& a, Vector<T, C>& b);
+            
         public:
             std::array<T, C> vec;
             Vector(std::initializer_list<T> list);
             virtual ~Vector();
     };
 
+    // template <typename T, size_t C>
+    // Vector<T, C> operator+(const T& rhs);
+    // template <typename T, size_t C>
+    // void operator+=(Vector<T, C> lhs, const T& rhs);
+
     template <typename T, size_t C>
-    Vector<T, C> operator+(const T& rhs);
-    template <typename T, size_t C>
-    void operator+=(Vector<T, C> lhs, const T& rhs);
+    
 }
