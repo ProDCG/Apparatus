dcef91e01c76435248a0bf7e7ac668b799acef66
Mason Stuart <mason.stuart05@outlook.com>
2022-09-15 22:13:20 -0700

    changing impl

--- Core/LinearAlgebra/Testing/matrix2.hpp
+++ Core/LinearAlgebra/Testing/matrix2.hpp
@@ -1,3 +1,9 @@
+#include <vector>
+
 namespace app {
-    
+    template <typename T, size_t C, size_t R>
+    class Matrix {
+        private:
+            std::array<std::array<T, C> R> mat;
+    }
 }
