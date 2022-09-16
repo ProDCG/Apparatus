04844f547f58d370c68401d4b58e37362bbe4e54
Mason Stuart <mason.stuart05@outlook.com>
2022-09-16 06:59:42 -0700

    Vector fix

--- Core/LinearAlgebra/Testing/vector2.hpp
+++ Core/LinearAlgebra/Testing/vector2.hpp
@@ -6,9 +6,9 @@
 namespace app {
     template <typename T, size_t C> class Vector{
         private:
-            
-        public:
             std::array<T, C> vec;
+        public:
+            
 
             Vector(std::initializer_list<T> list);
             virtual ~Vector();
@@ -16,9 +16,7 @@ namespace app {
             Vector<T, C> operator+(const T& rhs) {
                 std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
                     element += rhs;
-                })
+                });
             }
     };
 }
-
-#include <../Core/LinearAlgebra/Testing/vector2.cpp>
