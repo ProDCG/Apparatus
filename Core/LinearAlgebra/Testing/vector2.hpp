b14104a96573f4c6cc760ab3c8856f4a977f9d76
Mason Stuart <mason.stuart05@outlook.com>
2022-09-16 13:35:22 -0700

    Vector fixes

--- Core/LinearAlgebra/Testing/vector2.hpp
+++ Core/LinearAlgebra/Testing/vector2.hpp
@@ -7,7 +7,7 @@ namespace app {
     template <typename T, size_t C> class Vector{
         private:
             // std::array<T, C> vec;
-            Vector<T, C> operator+(Vector<T, C> lhs, Vector<T, C> rhs);
+            Vector<T, C> operator+(Vector<T, C> lhs, T& val);
             Vector<T, C> operator+=(Vector<T, C> rhs);
             
         public:
