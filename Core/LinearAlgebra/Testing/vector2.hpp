bf94b273f44b4d8c0429f7206f1abdf47ec17bb9
Mason Stuart <mason.stuart05@outlook.com>
2022-09-16 07:45:51 -0700

    Cleaned up vector2

--- Core/LinearAlgebra/Testing/vector2.hpp
+++ Core/LinearAlgebra/Testing/vector2.hpp
@@ -9,21 +9,10 @@ namespace app {
             std::array<T, C> vec;
         public:
             
-
             Vector(std::initializer_list<T> list);
             virtual ~Vector();
-
-            
     };
 
     template <typename T, size_t C>
     Vector<T, C> operator+(const T& rhs);
-
-    /*
-    {
-        std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
-            element += rhs;
-        });
-    }
-    */
 }
---
+++ Core/LinearAlgebra/Testing/vector_test.cpp
@@ -0,0 +1,6 @@
+#include <../Core/LinearAlgebra/Testing/vector2.hpp>
+
+int main() {
+    app::Vector<int, 3> vec3;
+    return 0;
+}
