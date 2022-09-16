ef61699e38116cb3a1251fb59085342aeda63237
Mason Stuart <mason.stuart05@outlook.com>
2022-09-15 22:18:08 -0700

    Added initializer lists

--- Core/LinearAlgebra/Testing/matrix2.hpp
+++ Core/LinearAlgebra/Testing/matrix2.hpp
@@ -1,9 +1,18 @@
-#include <vector>
+#include <array>
 
 namespace app {
     template <typename T, size_t C, size_t R>
     class Matrix {
         private:
-            std::array<std::array<T, C> R> mat;
-    }
+            std::array<std::array<T, C>, R> mat;
+        public:
+            Matrix(std::initializer_list<std::initializer_list<T>> list) {
+                auto element_it = mat.begin();
+
+                for (auto& row : list) {
+                    std::copy(row.begin(), row.end(), element_it -> begin());
+                    ++element_it;
+                }
+            }            
+    };
 }
