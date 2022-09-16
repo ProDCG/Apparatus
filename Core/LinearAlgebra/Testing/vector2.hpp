ec2065c0e5bc4fbae035f3b0f8002001211f79e5
Mason Stuart <mason.stuart05@outlook.com>
2022-09-16 10:31:53 -0700

    operator overloading

--- Core/LinearAlgebra/Testing/vector2.hpp
+++ Core/LinearAlgebra/Testing/vector2.hpp
@@ -7,7 +7,7 @@ namespace app {
     template <typename T, size_t C> class Vector{
         private:
             // std::array<T, C> vec;
-            Vector<T, C> operator+(Vector<T, C>& a, Vector<T, C>& b);
+            Vector<T, C> operator+(Vector<T, C> lhs, Vector<T, C> rhs);
             
         public:
             std::array<T, C> vec;
