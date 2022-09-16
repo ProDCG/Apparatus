5cc077f843b64951547da28953360114a9f719f3
Mason Stuart <mason.stuart05@outlook.com>
2022-09-16 07:52:10 -0700

    += impl

--- Core/LinearAlgebra/Testing/vector2.cpp
+++ Core/LinearAlgebra/Testing/vector2.cpp
@@ -2,11 +2,18 @@
 
 template <typename T, size_t C>
 app::Vector<T, C>::Vector(std::initializer_list<T> list) {
-    std::copy(list.begin(), list.end(), app::)
+    std::copy(list.begin(), list.end(), app::Vector<T, C>::vec);
 }
 
 template <typename T, size_t C>
 app::Vector<T, C> app::operator+(const T& rhs) {
+    std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
+        element += rhs;
+    });
+}
+
+template <typename T, size_t C>
+app::Vector<T, C> app::operator+=(const T& rhs) {
     std::for_each(vec.begin(), vec.end(), [rhs](auto& element) noexcept {
         element += rhs;
     });
--- Core/LinearAlgebra/Testing/vector_test.cpp
+++ Core/LinearAlgebra/Testing/vector_test.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     app::Vector<int, 3> vec3{1, 2, 3};
+    app::Vector<int, 3> vec2{4, 5, 6};
+    vec3 += vec2;
     return 0;
 }
