fb47f9ad1d7906d59b7681a688cd4a96b44b3a08
Mason Stuart <mason.stuart05@outlook.com>
2022-09-17 22:32:51 -0700

    single val constructor

--- Core/LinearAlgebra/Testing/vector2.cpp
+++ Core/LinearAlgebra/Testing/vector2.cpp
@@ -11,6 +11,13 @@ app::Vector<T, C>::Vector(std::array<T, C> arr) {
 }
 
 template <typename T, size_t C>
+app::Vector<T, C>::Vector(T& val) {
+    std::for_each(app::Vector<T, C>::vec.begin(), app::Vector<T, C>::vec.end(), [&](auto& value) noexcept {
+        value = val;
+    }) 
+}
+
+template <typename T, size_t C>
 app::Vector<T, C> app::Vector<T, C>::operator+(const T& rhs) {
     app::Vector<T, C> newVec(0.0f);
 
