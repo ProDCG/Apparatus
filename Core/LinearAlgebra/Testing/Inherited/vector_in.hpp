5f61a50c4df72c68cb1defdc5ddb0d7b11a5852c
Mason Stuart <mason.stuart05@outlook.com>
2022-09-20 13:31:25 -0700

    Inheriting constructor tests

--- Core/LinearAlgebra/Testing/Inherited/vector_in.hpp
+++ Core/LinearAlgebra/Testing/Inherited/vector_in.hpp
@@ -6,5 +6,11 @@
 
 template <size_t T, size_t C>
 class Vector : public std::array<T, C> {
-    
-}
+    public:
+        Vector();
+        Vector(std::initializer_list<T>);
+        Vector(double);
+        Vector(int);
+        Vector(std::array<T, C>);
+        Vector(Vector<T, C>);
+};
