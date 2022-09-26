badcef4f756ee6d7594875b34ac1de5e8f1dd4b2
Mason Stuart <mason.stuart05@outlook.com>
2022-09-26 10:02:32 -0700

    matrix constructor

--- Core/LinearAlgebra/Matrix/matrix_in.hpp
+++ Core/LinearAlgebra/Matrix/matrix_in.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <initializer_list>
+#include <array>
+#include <algorithm>
+#include <iostream>
+
 template <typename T, size_t C, size_t R>
 class Matrix : public std::array<std::array<T, C>, R> {
     public:
         Matrix();
 
-        Matrix(std::initializer_list<std::initializer_list<T>> list) {
+        Matrix(double num) {
 
         }
 
