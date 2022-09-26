38f369cf211aefc5eece29ca6e45993ae247a38f
Mason Stuart <mason.stuart05@outlook.com>
2022-09-26 07:59:46 -0700

    APP type definition

--- Core/LinearAlgebra/Testing/Inherited/vector_in_test.cpp
+++ Core/LinearAlgebra/Testing/Inherited/vector_in_test.cpp
@@ -2,7 +2,7 @@
 #include "vector_in_typedef.hpp"
 
 int main() {
-    vec3 cam = vec3(1.0f);
+    app::vec3 cam = app::vec3(1.0f);
     std::cout << cam[0] << std::endl;
     std::cout << cam[1] << std::endl;
     std::cout << cam[2] << std::endl;
--- Core/LinearAlgebra/Testing/Inherited/vector_in_typedef.hpp
+++ Core/LinearAlgebra/Testing/Inherited/vector_in_typedef.hpp
@@ -1,9 +1,11 @@
 #include "vector_in.hpp"
 
-typedef Vector<double, 2> vec2, dvec2;
-typedef Vector<double, 3> vec3, dvec3;
-typedef Vector<double, 4> vec4, dvec4;
+namespace app {
+    typedef Vector<double, 2> vec2, dvec2;
+    typedef Vector<double, 3> vec3, dvec3;
+    typedef Vector<double, 4> vec4, dvec4;
 
-typedef Vector<int, 2> ivec2;
-typedef Vector<int, 3> ivec3;
-typedef Vector<int, 4> ivec4;
+    typedef Vector<int, 2> ivec2;
+    typedef Vector<int, 3> ivec3;
+    typedef Vector<int, 4> ivec4;
+}
