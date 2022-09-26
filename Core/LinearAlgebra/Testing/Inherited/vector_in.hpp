64dda1f0e7f49322bbffd8d36fdc971dd04e3002
Mason Stuart <mason.stuart05@outlook.com>
2022-09-25 23:29:13 -0700

    .

--- Core/LinearAlgebra/Testing/Inherited/vector_in.hpp
+++ Core/LinearAlgebra/Testing/Inherited/vector_in.hpp
@@ -106,7 +106,7 @@ Vector<T, C> operator - (Vector<T, C> lhs, Vector<T, C> rhs) {
 
 template <typename T, size_t C>
 Vector<T, C> operator -= (Vector<T, C> lhs, Vector<T, C> rhs) {
-    
+         
 }
  
 template <typename T, size_t C>
