9d051f81fa6de2dcb1bd755442eb1736bd940487
Mason Stuart <mason.stuart05@outlook.com>
2022-09-25 23:31:18 -0700

    space removal

--- Core/LinearAlgebra/Testing/Non-Inherited/vector_test.cpp
+++ Core/LinearAlgebra/Testing/Non-Inherited/vector_test.cpp
@@ -2,10 +2,10 @@
 
 #include <iostream>
 
-int main() { 
+int main() {
     app::Vector<int, 3> vec3{1, 2, 3};
     app::Vector<int, 3> vec2{4, 5, 6};
     app::Vector<int, 3> vec1 = vec3 + vec2;
     vec3.print();
     return 0;
-}  
+}
