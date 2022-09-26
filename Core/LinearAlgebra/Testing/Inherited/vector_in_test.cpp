bfeeabcaaf74129ceb50a447e09392e11636c2aa
Mason Stuart <mason.stuart05@outlook.com>
2022-09-26 07:57:56 -0700

    Woooo! Type definitions work. Awesome

--- Core/LinearAlgebra/Testing/Inherited/vector_in_test.cpp
+++ Core/LinearAlgebra/Testing/Inherited/vector_in_test.cpp
@@ -1,17 +1,11 @@
 #include "vector_in.hpp"
+#include "vector_in_typedef.hpp"
 
 int main() {
-    Vector<double, 3> vec1{1.0, 2.0, 3.0};
-    Vector<double, 3> vec2{4, 5, 6};
-    Vector<double, 3> vec3 = vec2 + vec1;
-    std::cout << vec3[0] << '\n';
-    std::cout << vec3[1] << '\n';
-    std::cout << vec3[2] << '\n';
-    std::cout << magnitude(vec1) << '\n';
-    vec1 = powerFunc(vec1, 5);
-    std::cout << vec1[0] << '\n';
-    std::cout << vec1[1] << '\n';
-    std::cout << vec1[2] << '\n';
+    vec3 cam = vec3(1.0f);
+    std::cout << cam[0] << std::endl;
+    std::cout << cam[1] << std::endl;
+    std::cout << cam[2] << std::endl;
 
     return 0;
 }
---
+++ Core/LinearAlgebra/Testing/Inherited/vector_in_typedef.hpp
@@ -0,0 +1,5 @@
+#include "vector_in.hpp"
+
+typedef Vector<double, 2> vec2;
+typedef Vector<double, 3> vec3;
+typedef Vector<double, 4> vec4;
