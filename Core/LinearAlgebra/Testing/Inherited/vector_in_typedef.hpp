fe4551f451205d2d66e0d245a3d8ac7b0535f29f
Mason Stuart <mason.stuart05@outlook.com>
2022-09-26 07:58:54 -0700

    More type definitions

--- Core/LinearAlgebra/Testing/Inherited/vector_in_typedef.hpp
+++ Core/LinearAlgebra/Testing/Inherited/vector_in_typedef.hpp
@@ -1,5 +1,9 @@
 #include "vector_in.hpp"
 
-typedef Vector<double, 2> vec2;
-typedef Vector<double, 3> vec3;
-typedef Vector<double, 4> vec4;
+typedef Vector<double, 2> vec2, dvec2;
+typedef Vector<double, 3> vec3, dvec3;
+typedef Vector<double, 4> vec4, dvec4;
+
+typedef Vector<int, 2> ivec2;
+typedef Vector<int, 3> ivec3;
+typedef Vector<int, 4> ivec4;
