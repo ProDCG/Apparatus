713a1dd492aa6231fb9146079e539fa38e7b71f4
Mason Stuart <mason.stuart05@outlook.com>
2022-09-26 09:58:42 -0700

    Constructor changeS

--- Core/LinearAlgebra/Matrix/matrix_in.hpp
+++ Core/LinearAlgebra/Matrix/matrix_in.hpp
@@ -1,6 +1,12 @@
+#pragma once
+
 template <typename T, size_t C, size_t R>
 class Matrix : public std::array<std::array<T, C>, R> {
     public:
+        Matrix();
 
+        Matrix(std::initializer_list<std::initializer_list<T>> list) {
+
+        }
 
 };
