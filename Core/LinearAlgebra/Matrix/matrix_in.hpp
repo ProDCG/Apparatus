e53157d0592fb2498f01ce8eae76cd56c01752ae
Mason Stuart <mason.stuart05@outlook.com>
2022-09-26 22:14:37 -0700

    added matrix_in specific methods/constructors

--- Core/LinearAlgebra/Matrix/matrix_in.hpp
+++ Core/LinearAlgebra/Matrix/matrix_in.hpp
@@ -9,11 +9,14 @@ template <typename T, size_t C, size_t R>
 class Matrix : public std::array<std::array<T, C>, R> {
     public:
         Matrix();
-
         Matrix(double num) {
             int i = 0;
             std::for_each([0].begin(), [0].end(), [&](auto& element) noexcept {
                 std::cout << i << ' ' << std::endl;
             });
         }
+        Matrix(int num) {}
+        Matrix(std::initializer_list<std::initializer_list<T>>) {}
+
+        void print() const {}
 };
