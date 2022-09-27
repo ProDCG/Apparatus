1b02bae63c0b81980d7f0648fa6fbe8669373cdd
Mason Stuart <mason.stuart05@outlook.com>
2022-09-27 08:10:24 -0700

    -, *, / operators

--- Core/LinearAlgebra/Matrix/matrix_in.hpp
+++ Core/LinearAlgebra/Matrix/matrix_in.hpp
@@ -48,3 +48,23 @@ Matrix<T, C, R> operator - (Matrix<T, C, R> lhs, double scalar) {
     });
     return lhs;
 }
+
+template <typename T, size_t C, size_t R>
+Matrix<T, C, R> operator * (Matrix<T, C, R> lhs, double scalar) {
+    std::for_each(lhs.begin(), lhs.end(), [scalar](auto& row) noexcept {
+        std::for_each(row.begin(), row.end(), [scalar](auto& element) noexcept {
+            element *= scalar;
+        });
+    });
+    return lhs;
+}
+
+template <typename T, size_t C, size_t R>
+Matrix<T, C, R> operator / (Matrix<T, C, R> lhs, double scalar) {
+    std::for_each(lhs.begin(), lhs.end(), [scalar](auto& row) noexcept {
+        std::for_each(row.begin(), row.end(), [scalar](auto& element) noexcept {
+            element /= scalar;
+        });
+    });
+    return lhs;
+}
