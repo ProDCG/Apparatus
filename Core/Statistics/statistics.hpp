9a9b495fca7f5aacde0fb7577243354eec50e051
Mason Stuart <mason.stuart05@outlook.com>
2022-10-03 14:09:16 -0700

    normal dist formula

--- Core/Statistics/statistics.hpp
+++ Core/Statistics/statistics.hpp
@@ -3,6 +3,8 @@
 #include <array>
 #include <algorithm>
 #include <iostream>
+#include <cmath>
+#include <math.h>
 
 namespace app {
     double z_score(double std_dev, double observed, double average) {
@@ -10,6 +12,8 @@ namespace app {
     }
 
     double normal_distribution(double std_dev, double mean, double observed) {
-        
+        double exp = -std::pow(observed - mean, 2) / (2 * std::pow(std_dev, 2));
+        double base = 1 / (std_dev * std::sqrt(2 * std::atan(1)*4));
+        return base * std::pow(std::exp(1.0), exp);
     }
 }
