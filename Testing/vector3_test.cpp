3fee8e64fb58791ec08652329079a0e42216688c
Mason Stuart <mason.stuart05@outlook.com>
2023-01-26 09:59:26 -0800

    tested perpindicular vectors

--- Testing/vector3_test.cpp
+++ Testing/vector3_test.cpp
@@ -2,11 +2,18 @@
 #include <iostream>
 
 int main() {
+    // finite test
+    // perpindicular vector test
     app::vector3 vec3_1 = app::vector3(0.0/0.0, 36, 5);
     app::vector3 vec3_2 = app::vector3(4, 19, 16);
+
+    // perpindicular vector test
+    app::vector3 vec3_3 = app::vector3(0, 6, 0);
+    app::vector3 vec3_4 = app::vector3(5, 0, 0);
     
     std::cout << "V1 isFinite: " << vec3_1.isFinite() << std::endl;
     std::cout << "V2 isPerpindicular: " << vec3_1.isPerpindicular(vec3_2) << std::endl;
+    std::cout << "V3 isPerpindicular: " << vec3_3.isPerpindicular(vec3_4) << std::endl;
 
     return 0;
 }
